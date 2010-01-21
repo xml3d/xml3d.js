34731a734c3bdfcb08948293add67c5082155a9b
Felix Klein <lachsen@graphics.cs.uni-sb.de>
2010-01-21 18:48:16 +0100

    Updated Scene Controller + Conformance Test for View Element
    - scene controller uses View Element with id "controller_view"
    - conformance tests contain view element to keep old camera position
    - added conformance test 13 for view element.

--- script/xml3d_scene_controller.js
+++ script/xml3d_scene_controller.js
@@ -11,25 +11,41 @@ else if (typeof org.xml3d != "object")
 
 org.xml3d.Xml3dSceneController = function(canvas, xml3d) {
 	var self = this;
-	
 	this.buildIn = xml3d.style !== undefined;
 
 	this.xml3d = this.buildIn ? canvas : xml3d;
 	this.canvas = canvas;
-	this.camera = xml3d.camera;
-
+	this.camera = document.getElementById("controller_view");
 	var InputPanelId = null;
 
 	this.old_x = -1;
 	this.old_y = -1;
+	this.yaw = 0;
+	this.pitch = 0;
+	
+	this.createVec3 = function(x, y, z){
+		var res = this.xml3d.createXML3DVec3();
+		res.x = x; res.y = y; res.z = z;
+		return res;
+	};
 
-	this.UPVECTOR = new org.xml3d.dataTypes.Vec3f(0.0, 1.0, 0.0);
-	this.ZVECTOR = new org.xml3d.dataTypes.Vec3f(0.0, 0.0, -1.0);
+	if(this.buildIn)
+	{
+		this.UPVECTOR = this.createVec3(0, 1, 0);
+		this.ZVECTOR = this.createVec3(0, 0, -1);	}
+	else
+	{
+		this.UPVECTOR = new org.xml3d.dataTypes.Vec3f(0.0, 1.0, 0.0);
+		this.ZVECTOR = new org.xml3d.dataTypes.Vec3f(0.0, 0.0, -1.0);	
+	}
 
 	this.moveSpeedElement = document.getElementById("moveSpeed");
 
-	if (!this.xml3d || !this.xml3d.camera)
+	if (!this.xml3d || !this.camera)
+	{
 		return;
+	}
+		
 
 	
 	this.rotateSpeed = 0.005;
@@ -173,18 +189,10 @@ org.xml3d.Xml3dSceneController.prototype.manageDrag = function(event) {
 org.xml3d.Xml3dSceneController.prototype.dolly = function(ev) {
 
 	var dy = (ev.pageY - this.old_y);
-
-	if (this.camera.direction !== undefined) {
-		var up = this.camera.up;
-		up.x = up.z = 0;
-		up.y = 1;
-
-		this.camera.position.x += (this.camera.direction.x * -dy);
-		this.camera.position.y += (this.camera.direction.y * -dy);
-		this.camera.position.z += (this.camera.direction.z * -dy);
-
+	if(this.camera.getDirection !== undefined){
+		var dir = this.camera.getDirection();
+		this.camera.position = this.camera.position.add(dir.scale(-dy * 0.5));
 	} else {
-
 		var direction = this.camera.orientation.rotateVec(this.ZVECTOR)
 				.normalised();
 		this.camera.position = this.camera.position.add(direction
@@ -201,33 +209,42 @@ org.xml3d.Xml3dSceneController.prototype.rotate = function(ev) {
 	var dx = (ev.pageX - this.old_x) * this.rotateSpeed;
 	var dy = (ev.pageY - this.old_y) * this.rotateSpeed;
 
-	if (this.camera.direction !== undefined) {
-		var up = this.camera.up;
-		up.x = up.z = 0;
-		up.y = 1;
+	if (this.camera.getDirection !== undefined) {
 
-		var direction = this.camera.direction;
+		var dir = this.camera.getDirection();
+		
 		if (dx) {
-			var s = Math.sin(dx);
-			var c = Math.cos(dx);
-			var length1 = Math.sqrt(direction.x * direction.x + direction.z
-					* direction.z);
-			direction.x = c * direction.x - s * direction.z;
-			direction.z = s * direction.x + c * direction.z;
-			length1 /= Math.sqrt(direction.x * direction.x + direction.z
-					* direction.z);
-			direction.x = direction.x * length1;
-			direction.z = direction.z * length1;
+			this.yaw -= dx;
+			/*
+			if(this.yaw < 0)
+				this.yaw += 360;
+			if(this.yaw > 360)
+				this.yaw -= 360;
+			*/
 		}
 		if (dy) {
-			var rotVec = direction.cross(up);
-			var m = org.xml3d.dataTypes.SFMatrix4.parseRotation(rotVec.x + " "
-					+ rotVec.y + " " + rotVec.z + " " + dy);
-			var newdirection = m.multMatrixVec(direction);
-			direction.x = newdirection.x;
-			direction.y = newdirection.y;
-			direction.z = newdirection.z;
+			this.pitch -= dy;
+			
+			if(this.pitch < -1.5)
+				this.pitch = -1.5;
+			if(this.pitch > 1.5)
+				this.pitch = 1.5;
 		}
+		var newdir = this.ZVECTOR;
+		var rot1 = this.ZVECTOR.cross(this.UPVECTOR);
+		var m = org.xml3d.dataTypes.SFMatrix4.parseRotation(rot1.x + " "
+				+ rot1.y + " " + rot1.z + " " + this.pitch);
+		var newdir = m.multMatrixVec(newdir);
+		
+		var rot2 = this.UPVECTOR;
+		var m = org.xml3d.dataTypes.SFMatrix4.parseRotation(rot2.x + " "
+				+ rot2.y + " " + rot2.z + " " + this.yaw);
+		var newdir = m.multMatrixVec(newdir);
+		dir.x = newdir.x;
+		dir.y = newdir.y;
+		dir.z = newdir.z;
+		this.camera.setUpVector(this.UPVECTOR);
+		this.camera.setDirection(dir);
 	} else {
 		var direction = this.camera.orientation.rotateVec(this.ZVECTOR)
 				.normalised();
@@ -285,33 +302,29 @@ org.xml3d.Xml3dSceneController.prototype.keyHandling = function(e) {
 
 	var xml3d = this.xml3d;
 	// alert(xml3d);
-	var camera = xml3d.camera;
+	var camera = this.camera;
+	var dir = camera.getDirection();
 	if (xml3d) {
 		switch (KeyID) {
 		case 38: // up
 		case 87: // w
-			xml3d.camera.position.x += camera.direction.x * this.getMoveScale();
-			xml3d.camera.position.y += camera.direction.y * this.getMoveScale();
-			xml3d.camera.position.z += camera.direction.z * this.getMoveScale();
+			camera.position = camera.position.add(dir.scale(this.getMoveScale()));
 			break;
 		case 39: // right
 		case 68: // d
-			xml3d.camera.position.x -= camera.direction.z * this.getMoveScale();
-			xml3d.camera.position.z += camera.direction.x * this.getMoveScale();
+			camera.position.x -= dir.z * this.getMoveScale();
+			camera.position.z += dir.x * this.getMoveScale();
 
 			break;
 		case 37: // left
 		case 65: // a
-			xml3d.camera.position.x += camera.direction.z * this.getMoveScale();
-			xml3d.camera.position.z -= camera.direction.x * this.getMoveScale();
+			camera.position.x += dir.z * this.getMoveScale();
+			camera.position.z -= dir.x * this.getMoveScale();
 
 			break;
 		case 40: // down
 		case 83: // s
-			xml3d.camera.position.x -= camera.direction.x * this.getMoveScale();
-			xml3d.camera.position.y -= camera.direction.y * this.getMoveScale();
-			xml3d.camera.position.z -= camera.direction.z * this.getMoveScale();
-
+			camera.position = camera.position.subtract(dir.scale(this.getMoveScale()));
 			break;
 
 		default:

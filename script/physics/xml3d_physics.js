c9c1f495613d08666f6e5a6c7c9287f12c3de142
Kristian Sons <kristian.sons@dfki.de>
2010-12-14 10:40:51 +0100

    Prepared iX physics examples

--- script/physics/xml3d_physics.js
+++ script/physics/xml3d_physics.js
@@ -1,3 +1,25 @@
+/*************************************************************************/
+/*                                                                       */
+/*  xml3d_physics.js                                                     */
+/*  Communication with physics simulation plugin 						 */
+/*                                                                       */
+/*  Copyright (C) 2010                                                   */
+/*  DFKI - German Research Center for Artificial Intelligence            */
+/*                                                                       */
+/*  This file is part of xml3d.js                                        */
+/*                                                                       */
+/*  xml3d.js is free software; you can redistribute it and/or modify     */
+/*  under the terms of the GNU General Public License as                 */
+/*  published by the Free Software Foundation; either version 2 of       */
+/*  the License, or (at your option) any later version.                  */
+/*                                                                       */
+/*  xml3d.js is distributed in the hope that it will be useful, but      */
+/*  WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                 */
+/*  See the GNU General Public License                                   */
+/*  (http://www.fsf.org/licensing/licenses/gpl.html) for more details.   */
+/*                                                                       */
+/*************************************************************************/
 //-------------------------------------------------------------------
 // XML3D Physics loader
 //-------------------------------------------------------------------

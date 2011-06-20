50c387fc497d1206eba286780bcc3003fdf41c8c
Christian Schlinkmann <s9cnschl@stud.uni-saarland.de>
2011-06-20 22:48:32 +0200

    Added skinning related XFlow scripts, Fixed data src references in skinning conformance tests

--- script/xml3d_renderer.js
+++ script/xml3d_renderer.js
@@ -2311,13 +2311,8 @@ org.xml3d.webgl.XML3DMeshRenderAdapter.prototype.notifyChanged = function(e) {
 };
 
 org.xml3d.webgl.XML3DMeshRenderAdapter.prototype.notifyDataChanged = function(e) {
-	/*if (this.loadedMesh) {
-		var dt = this.dataAdapter.createDataTable();
-		var newData = dt[e.notifier.name];
-		if (newData)
-			this.mesh.addVertexAttribute(e.notifier.name, newData.tupleSize, newData.data);
-	}*/
-	this.loadedMesh = false;
+		
+	//this.loadedMesh = false;
 	this.factory.renderer.requestRedraw("Mesh data has changed");
 };
 
@@ -2420,7 +2415,7 @@ org.xml3d.webgl.XML3DMeshRenderAdapter.prototype.render = function(shader, param
 			if (p == "index")
 				continue;
 			
-			if (p == "xflowShader" || p=="segments") {
+			if (meshParams[p].isXFlow || p == "xflowShader") {
 				continue;
 			}
 			
@@ -4187,50 +4182,7 @@ org.xml3d.xflow.ripple = function(dataTable) {
 	delete dataTable.strength;
 	delete dataTable.wavelength;
 	delete dataTable.phase;
-	
-	
-	/*var position = dataTable.position.data;
-	if (dataTable.normal)
-		var normal = dataTable.normal.data;
-	
-	var strength = dataTable.strength.data[0];
-	var wavelength = dataTable.wavelength.data[0];
-	var phase = dataTable.phase.data[0];
-	
-	var numVertices = position.length / 3;
-	var newPos = new Float32Array(numVertices*3); 
-	if (normal)
-		var newNorm = new Float32Array(numVertices*3); 
-	
-	for (var i=0; i<numVertices; i++) {
-		var index = i*3;
-		var curPos = new XML3DVec3(position[index], position[index+1], position[index+2]);
-		var dist = Math.sqrt(curPos.x*curPos.x + curPos.z*curPos.z);
-		var height = Math.sin(dist * wavelength + phase)*strength;
-		curPos.y += height;
-		
-		newPos.set([curPos.x, curPos.y, curPos.z], index);
-		
-		if (normal) {
-			var dx = Math.cos(dist*wavelength*phase) * strength*wavelength/dist*curPos.x;
-			var dz = Math.cos(dist*wavelength*phase) * strength*wavelength/dist*curPos.z;
-			if (isNaN(dx) || isNaN(dz)) {
-				newNorm.set([normal[index], normal[index+1], normal[index+2]], index);
-				continue;
-			}
-			var u = new XML3DVec3(-1, -dx+dz, 1);
-			var v = new XML3DVec3(1, dx+dz, 1);
-			var curNorm = new XML3DVec3(normal[index], normal[index+1], normal[index+2]);
-			curNorm = u.cross(v).normalize();
-			newNorm.set([curNorm.x, curNorm.y, curNorm.z], index);
-		}
-	}
-	
-	dataTable.position = { data : position, tupleSize : 3};
-	if (normal)
-		dataTable.normal = { data : normal, tupleSize : 3 };
-	
-	return;*/
+
 };
 
 org.xml3d.xflow.morphing = function(dataTable) {
@@ -4311,6 +4263,43 @@ org.xml3d.xflow.noise = function(dataTable) {
 	
 };
 
+org.xml3d.xflow.displace = function(dataTable) {
+
+	//TODO: Texture lookup in vertex shader is not yet supported in WebGL
+	delete dataTable.diffuseTexture;
+	delete dataTable.strength;
+	return;
+	
+	if (!dataTable.strength || !dataTable.diffuseTexture) {
+		org.xml3d.debug.logError("Missing parameters for XFlow Displace script!");
+		return;
+	}
+	
+	var sd = "uniform sampler2D diffuseTexture;\n"
+		+ "uniform float strength;\n"
+		+ "attribute vec2 texcoord;\n";
+	
+	var sb = "vec4 d = texture2D(diffuseTexture, texcoord);\n";
+	sb += "pos += norm * strength * ((d.x + d.y + d.z) / 3.0 * d.w);\n";
+	
+	if (dataTable.xflowShader) {
+		dataTable.xflowShader.declarations += sd;
+		dataTable.xflowShader.body += sb;
+	} else {
+		dataTable.xflowShader = {};
+		dataTable.xflowShader.body = sb;
+		dataTable.xflowShader.declarations = sd;
+		dataTable.xflowShader.body = sb;
+		dataTable.xflowShader.uniforms = {};		
+	}
+	
+	dataTable.xflowShader.uniforms["strength"] = dataTable.strength;
+	delete dataTable.strength;
+	dataTable.xflowShader.uniforms["diffuseTexture"] = dataTable.diffuseTexture;
+	delete dataTable.diffuseTexture;
+	
+};
+
 org.xml3d.xflow.smoothing = function(dataTable) {
 	//Can't do smoothing in a vertex shader as it's not parallel
 	
@@ -4372,3 +4361,310 @@ org.xml3d.xflow.uv = function(dataTable) {
 	delete dataTable.translate;
 };
 
+org.xml3d.xflow.tangent = function(dataTable) {
+	
+	var numVertices = dataTable.position.data.length / 3;
+	var numTriangles = dataTable.index.data.length / 3;
+	var tangents = new Float32Array(numVertices*3);
+	
+	var tan1 = new Float32Array(numVertices*3);
+	
+	for (var i = 0; i<numTriangles; i++) {
+	try {
+		var index0 = dataTable.index.data[i*3];
+		var index1 = dataTable.index.data[i*3 + 1];
+		var index2 = dataTable.index.data[i*3 + 2];
+		
+		var pos1 = new XML3DVec3(dataTable.position.data[index0], dataTable.position.data[index0+1],
+				dataTable.position.data[index0+2]);
+		var pos2 = new XML3DVec3(dataTable.position.data[index1], dataTable.position.data[index1+1],
+				dataTable.position.data[index1+2]);
+		var pos3 = new XML3DVec3(dataTable.position.data[index2], dataTable.position.data[index2+1],
+				dataTable.position.data[index2+2]);
+		var q1 = pos2.subtract(pos1);
+		var q2 = pos3.subtract(pos1);
+		
+		var ti0 = 2*index0;
+		var ti1 = 2*index1;
+		var ti2 = 2*index2;
+		
+		var tex1 = new XML3DVec3(dataTable.texcoord.data[ti0], dataTable.texcoord.data[ti0+1], 0);
+		var tex2 = new XML3DVec3(dataTable.texcoord.data[ti1], dataTable.texcoord.data[ti1+1], 0);
+		var tex3 = new XML3DVec3(dataTable.texcoord.data[ti2], dataTable.texcoord.data[ti2+1], 0);
+		var u1 = tex2.subtract(tex1);
+		var u2 = tex3.subtract(tex1);
+		
+		var r = 1.0 / (u1.x * u2.y - u2.x * u1.y);
+		var sdir = new XML3DVec3( (u2.y*q1.x - u1.y*q2.x)*r, (u2.y*q1.y - u1.y*q2.y)*r, (u2.y*q1.z - u1.y*q2.z)*r );
+		var tdir = new XML3DVec3( (u1.x*q2.x - u2.x*q1.x)*r, (u1.x*q2.y - u2.x*q1.y)*r, (u1.x*q2.z - u2.x*q1.z)*r );
+		
+		tan1.set([ tan1[index0]+sdir.x, tan1[index0+1]+sdir.y, tan1[index0+2]+sdir.z ], index0);
+		tan1.set([ tan1[index1]+sdir.x, tan1[index1+1]+sdir.y, tan1[index1+2]+sdir.z ], index1);
+		tan1.set([ tan1[index2]+sdir.x, tan1[index2+1]+sdir.y, tan1[index2+2]+sdir.z ], index2);
+
+	}catch(e) {
+	}
+	}
+	
+	for (var i = 0; i<numVertices; i++) {
+		try {
+		var n = new XML3DVec3(dataTable.normal.data[i], dataTable.normal.data[i+1],
+				dataTable.normal.data[i+2]);
+		var t = new XML3DVec3(tan1[i], tan1[i+1], tan1[i+2]);
+		//var t2 = new XML3DVec3(tan2[i], tan2[i+1], tan2[i+2]);
+		
+		var tangent = (t.subtract(n).scale(n.dot(t))).normalize();
+		tangents.set(tangent.toGL(), i);
+		} catch (e) {
+			var ef = e;
+		}
+
+	}
+	
+	dataTable.tangent = { data : tangents, tupleSize : 3 };
+
+};
+
+org.xml3d.xflow.skinning = function(dataTable, dataAdapter) {
+	if (!dataTable.bindPose || !dataTable.boneIndices || !dataTable.boneWeights || !dataTable.pose || !dataTable.normal) {
+		org.xml3d.debug.logError("Missing parameters for XFlow Skinning script!");
+		return;
+	}
+	dataTable.bindPose.isXFlow = true;
+	dataTable.boneIndices.isXFlow = true;
+	dataTable.boneWeights.isXFlow = true;
+	dataTable.pose.isXFlow = true;
+	
+	var bindPose = new Array();
+	var pose = new Array();
+	var numMatrices = dataTable.bindPose.data.length / 16;
+	
+	if (dataTable.pose.data.length != dataTable.bindPose.data.length)
+		return;
+	
+	
+	
+	var sd = "uniform mat4 pose["+numMatrices+"];\n"
+		+ "uniform mat4 bindPose["+numMatrices+"];\n"
+		+ "attribute vec4 boneIndex;\n"
+		+ "attribute vec4 boneWeight;\n";
+	var sb = "";
+	
+	sb += "vec4 nPos = vec4(0.0);\n";
+	sb += "vec4 nNorm = vec4(0.0);\n";
+	sb += "vec4 index = boneIndex;\n";
+	sb += "vec4 weight = boneWeight;\n";
+	
+	sb += "for (int i = 0; i<4; i++) { \n";
+	sb += "   if (index.x < "+numMatrices+".0) {\n";
+	sb += "      vec4 bindPos =  bindPose[int(index.x)] * vec4(position.xyz, 1.0);\n";
+	sb += "      vec4 bindNorm = bindPose[int(index.x)] * vec4(normal.xyz, 0.0);\n";
+	sb += "      vec4 posePos = pose[int(index.x)] * vec4(bindPos.xyz, 1.0);\n";
+	sb += "      vec4 poseNorm = pose[int(index.x)] * vec4(bindNorm.xyz, 0.0);\n";
+	sb += "      nPos += posePos * weight.x;\n";
+	sb += "      nNorm += poseNorm * weight.x;\n";
+	sb += "   }\n";
+	sb += "   index = index.yzwx;\n";
+	sb += "   weight = weight.yzwx;\n";
+	sb += "}\n";
+	
+	sb += "float restWeight = 1.0 - (boneWeight.x + boneWeight.y + boneWeight.z + boneWeight.w);\n";
+	sb += "nPos = nPos + vec4(position, 0.0) * restWeight;\n";
+	sb += "nNorm = nNorm + vec4(normal, 0.0) * restWeight;\n";
+	
+	sb += "pos = nPos.xyz;\n";
+	sb += "norm = nNorm.xyz;\n";
+
+	
+	if (dataTable.xflowShader) {
+		dataTable.xflowShader.declarations += sd;
+		dataTable.xflowShader.body += sb;
+	} else {
+		dataTable.xflowShader = {};
+		dataTable.xflowShader.body = sb;
+		dataTable.xflowShader.declarations = sd;
+		dataTable.xflowShader.body = sb;
+		dataTable.xflowShader.uniforms = {};		
+	}	
+	
+	
+	dataTable.xflowShader.uniforms["pose[0]"] = dataTable.pose;
+	dataTable.xflowShader.uniforms["bindPose[0]"] = dataTable.bindPose;
+	dataTable.boneIndex = { data : new Uint16Array(dataTable.boneIndices.data), tupleSize : 4 };
+	//delete dataTable.boneIndices;
+	dataTable.boneWeight = { data : dataTable.boneWeights.data, tupleSize : 4 };
+};
+
+org.xml3d.xflow.forwardkinematics = function(dataTable) {
+	if (!dataTable.parent || !dataTable.transform) {
+		org.xml3d.debug.logError("Missing parameters for XFlow Forward Kinematics script!");
+		return;
+	}
+	dataTable.parent.isXFlow = true;
+	dataTable.transform.isXFlow = true;
+	var parent = dataTable.parent.data;
+	var transform = new Array();
+	var numJoints = dataTable.transform.data.length / 16;
+	var newPose = new Array();
+	
+	for (var i=0; i < numJoints*16;) {
+		var matTransform = new XML3DMatrix(dataTable.transform.data.subarray(i, i+16));
+		transform.push(matTransform);
+		newPose.push(new XML3DMatrix());
+		i+=16;
+	}
+	
+	if (parent.length != numJoints)
+		return;
+	
+	for (var i=0; i < numJoints; i++) {
+		var parentIndex = parent[i];
+		var curParentMatrix = new XML3DMatrix();
+		
+		if ( (parentIndex >= 0) && (parentIndex < numJoints)) {
+			curParentMatrix = newPose[parentIndex];
+		}
+		
+		var curMatrix = transform[i];
+		curMatrix = curMatrix.multiply(curParentMatrix);
+		
+		newPose[i] = curMatrix;
+	}
+	var newPoseArray = new Float32Array(dataTable.transform.data.length);
+	for (var i=0; i<numJoints; i++) {
+		newPoseArray.set(newPose[i].transpose().toGL(), i*16);
+	}
+	
+	dataTable.pose = { data : newPoseArray, tupleSize : 16 };
+	
+};
+
+org.xml3d.xflow.matrixinterpolator = function(dataTable) {
+	if (!dataTable.weight) {
+		org.xml3d.debug.logError("Missing parameters for XFlow Matrix Interpolator script!");
+		return;
+	}
+	dataTable.weight.isXFlow = true;
+	var weights = dataTable.weight.data;
+	//var transform = dataTable.transform.data;
+	
+	var weightValue = weights[0];
+	var index1 = Math.floor(weightValue);
+	var index2 = index1 + 1;
+	
+	
+	
+	var p1 = "transform"+index1;
+	var p2 = "transform"+index2;
+	
+	var pose1 = dataTable[p1].data;
+	var pose2 = dataTable[p2].data;
+	
+	if (pose1.length != pose2.length)
+		return;
+	
+	var newPose = new Float32Array(pose1.length);
+	var numMatrices = pose1.length / 16;
+	
+	var bv = weightValue - index1;
+	var onembv = 1 - bv;
+	
+	for (var i=0; i < numMatrices*16;) {
+		newPose[i] = pose1[i] * onembv + pose2[i] * bv;
+		newPose[i+1] = pose1[i+1] * onembv + pose2[i+1] * bv;
+		newPose[i+2] = pose1[i+2] * onembv + pose2[i+2] * bv;
+		newPose[i+3] = pose1[i+3] * onembv + pose2[i+3] * bv;
+		newPose[i+4] = pose1[i+4] * onembv + pose2[i+4] * bv;
+		newPose[i+5] = pose1[i+5] * onembv + pose2[i+5] * bv;
+		newPose[i+6] = pose1[i+6] * onembv + pose2[i+6] * bv;
+		newPose[i+7] = pose1[i+7] * onembv + pose2[i+7] * bv;
+		newPose[i+8] = pose1[i+8] * onembv + pose2[i+8] * bv;
+		newPose[i+9] = pose1[i+9] * onembv + pose2[i+9] * bv;
+		newPose[i+10] = pose1[i+10] * onembv + pose2[i+10] * bv;
+		newPose[i+11] = pose1[i+11] * onembv + pose2[i+11] * bv;
+		newPose[i+12] = pose1[i+12] * onembv + pose2[i+12] * bv;
+		newPose[i+13] = pose1[i+13] * onembv + pose2[i+13] * bv;
+		newPose[i+14] = pose1[i+14] * onembv + pose2[i+14] * bv;
+		newPose[i+15] = pose1[i+15] * onembv + pose2[i+15] * bv;
+		
+		i += 16;
+	}
+	
+	dataTable.transform = { data : newPose, tupleSize : 16 };
+	for (var i=0;;i++) {
+		if (dataTable["transform"+i]) {
+			delete dataTable["transform"+i];
+		}
+		else
+			break;
+	}
+	
+};
+
+org.xml3d.xflow.instance = function(dataTable) {
+	
+	if ((!dataTable.pose && !dataTable.transform) || !dataTable.texcoord || !dataTable.index) {
+		org.xml3d.debug.logError("Missing parameters for XFlow Instance script!");
+		return;
+	}
+	
+	if (dataTable.transform && !dataTable.pose) {
+		dataTable.pose = dataTable.transform;
+	}
+	dataTable.pose.isXFlow = true;
+	var index = dataTable.index.data;
+	var position = dataTable.position.data;
+	var normal = dataTable.normal.data;
+	var texcoord = dataTable.texcoord.data;
+	var pose = dataTable.pose.data;
+	var size = 1;
+	if (dataTable.size) {
+		size = dataTable.size.data[0];
+	}
+	
+	var numIndices = index.length;
+	var numVertices = position.length / 3;
+	var numInstances = pose.length / 16;
+	
+	var newIndex = new Int32Array(numIndices * numInstances);
+	var newPos = new Float32Array(numVertices*3 * numInstances);
+	var newNorm = new Float32Array(numVertices*3 * numInstances);
+	var newTexcoord = new Float32Array(numVertices*2 * numInstances);
+	
+	for (var j=0; j<numInstances; j++) {
+		var matrix = new XML3DMatrix(pose.subarray(j*16, (j*16)+16)).transpose();
+		
+		for (var i=0; i < numIndices; i++) {
+			var curIndex = index[i];
+			curIndex += j * numVertices;
+			
+			var instanceIndex = j * numIndices + i;
+			
+			newIndex.set([curIndex], instanceIndex);
+		}	
+		
+		for (var i=0; i < numVertices; i++) {
+			var curPos = new XML3DVec3(position[i*3], position[i*3+1], position[i*3+2]);
+			var curNorm = new XML3DVec3(normal[i*3], normal[i*3+1], normal[i*3+2]);
+			
+			var transformedPos = matrix.mulVec3b(curPos, 1).scale(size);
+			var transformedNorm = matrix.mulVec3b(curNorm, 1);
+			
+			var instanceIndex = j * numVertices*3 + i*3;
+			var texindex = j * numVertices*2 + i*2;
+			
+			newPos.set(transformedPos.toGL(), instanceIndex);
+			newNorm.set(transformedNorm.normalize().toGL(), instanceIndex);
+			newTexcoord.set([texcoord[i*2], texcoord[i*2+1]], texindex);
+		}	
+	}
+	
+	dataTable.index = { data : newIndex, tupleSize : 1 };
+	dataTable.position = { data : newPos, tupleSize : 3 };
+	dataTable.normal = { data : newNorm, tupleSize : 3 };
+	dataTable.texcoord = { data : newTexcoord, tupleSize : 2 };
+	if (dataTable.size)
+		delete dataTable.size;
+	
+
+};

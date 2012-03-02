e9557d46da8520f024162fc8a1365983d5977b35
Kristian Sons <ksons@cg.uni-saarland.de>
2012-03-02 22:03:11 +0100

    Made test wait for rendering finished.

--- tests/rendering-webgl.js
+++ tests/rendering-webgl.js
@@ -29,20 +29,35 @@ test("Background and invisible mesh", 4, function() {
     deepEqual(actual, [0,0,0,0], "Transparent at 0,0");
 });
 
-test("Change visibility via script", 6, function() {
-    var x = this.doc.getElementById("xml3DElem"), actual, win = this.doc.defaultView;
-    var gl = getContextForXml3DElement(x);
+test("Change visibility via script", 7, function() {
+    var x = this.doc.getElementById("xml3DElem"),
+        actual,
+        win = this.doc.defaultView,
+        gl = getContextForXml3DElement(x),
+        testFunc = null;
 
+    // Should be
+    /*x.addEventListener("framedrawn", function() {
+    console.log("Frame drawn");
+    });*/
+
+    x._configured.adapters.XML3DRenderAdapterFactory.handler.events.framedrawn.push({
+        listener : function(n) {
+            ok("Redraw was triggered");
+            if(testFunc)
+                testFunc(n);
+            start();
+        }
+    });
+
+    testFunc = function(n) {
+        equal(n.numberOfObjectsDrawn, 1, "1 Object drawn");
+        equal(n.numberOfTrianglesDrawn, 2, "2 Triangles drawn");
+        actual = win.getPixelValue(gl, 40, 40);
+        deepEqual(actual, [0,0,255,255], "Blue at 40,40");
+        actual = win.getPixelValue(gl, 0, 0);
+        deepEqual(actual, [0,0,0,0], "Transparent at 0,0");
+    };
+    stop();
     this.doc.getElementById("myGroup").visible = true;
-
-    actual = win.getPixelValue(gl, 40, 40);
-    deepEqual(actual, [0,0,255,255], "Blue at 40,40");
-    actual = win.getPixelValue(gl, 0, 0);
-    deepEqual(actual, [0,0,0,0], "Transparent at 0,0");
-
-    this.doc.getElementById("myGroup").setAttribute("visible", "false");
-    actual = win.getPixelValue(gl, 40, 40);
-    deepEqual(actual, [0,0,0,0], "Transparent at 40,40");
-    actual = win.getPixelValue(gl, 0, 0);
-    deepEqual(actual, [0,0,0,0], "Transparent at 0,0");
 });

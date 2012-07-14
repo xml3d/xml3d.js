f364a2ce4ba03856f153b150a89b90df7f4e1b07
Kristian Sons <ksons@cg.uni-saarland.de>
2012-07-14 15:03:55 +0200

    Fixed bug in shadermanager

--- src/renderer/shadermanager.js
+++ src/renderer/shadermanager.js
@@ -13,36 +13,36 @@
             LOADED: 1,
             VALID: 2
     };
-    
+
     var TextureInfo = function(opt) {
         opt = opt || {};
         this.status = TEXTURE_STATE.INVALID;
         this.onload = opt.onload;
     };
-    
+
     TextureInfo.prototype.createEmpty = function(gl, unit, image) {
         this.status = TEXTURE_STATE.UNLOADED;
         this.handle =  gl.createTexture();
         this.unit = unit;
         this.image = image;
     };
-    
+
     TextureInfo.prototype.setLoaded = function() {
         if(this.status != TEXTURE_STATE.UNLOADED)
             XML3D.debug.logError("Trying to set Texture with state " + this.status + " to 'loaded'" );
-        this.status = TEXTURE_STATE.LOADED; 
+        this.status = TEXTURE_STATE.LOADED;
         if(this.onload)
             this.onload.call(this);
     };
-    
+
     TextureInfo.prototype.setOptions = function(opt) {
         this.options = opt;
     };
-    
+
     var InvalidTexture = function() {
         this.status = TEXTURE_STATE.INVALID;
     };
-    
+
     var EMPTY_TEXTURE = null;
     var createEmptyTexture = function(gl) {
         var handle =  gl.createTexture();
@@ -53,7 +53,7 @@
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         EMPTY_TEXTURE = handle;
     };
-    
+
     var XML3DShaderManager = function(gl, renderer, dataFactory, factory) {
     	this.gl = gl;
     	this.renderer = renderer;
@@ -61,9 +61,9 @@
     	this.factory = factory;
     	this.currentProgram = null;
     	this.shaders = {};
-    
+
     	createEmptyTexture(gl);
-    	
+
     	//Always create a default flat shader as a fallback for error handling
     var fallbackShader = this.getStandardShaderProgram("matte");
     	fallbackShader.hasTransparency = false;
@@ -71,7 +71,7 @@
     	this.setUniform(gl, fallbackShader.uniforms["diffuseColor"], [1, 0, 0]);
     	this.unbindShader(fallbackShader);
     	this.shaders["defaultShader"] = fallbackShader;
-    
+
     	//Create picking shaders
     this.shaders["picking"] = this.getStandardShaderProgram("picking");
     this.shaders["pickedNormals"] = this.getStandardShaderProgram("pickedNormals");
@@ -82,20 +82,20 @@
     	var shader = null;
     	var shaderId = "defaultShader";
     	var shaderNode = null;
-    
+
     	if (shaderAdapter) {
     		shaderNode = shaderAdapter.node;
     		shaderId = shaderNode.id;
     	}
-    
+
         shader = this.shaders[shaderId];
-    
+
         if (shader)
             return shaderId;
-    
+
     var sources = {vertex:null, fragment:null};
         var flags = {hasTextures : false, hasTransparency : false, hasVColors : false};
-    
+
         if (shaderAdapter) {
             var dataTable = shaderAdapter.requestData(["transparency", "diffuseTexture", "specularTexture", "normalTexture", "useVertexColor"]);
     	    if (dataTable.transparency) {
@@ -104,7 +104,7 @@
     	    flags.hasTextures = dataTable.diffuseTexture !== undefined;
     	    flags.hasVColors = dataTable.useVertexColor && dataTable.useVertexColor.value[0] == true;
         }
-    
+
     	if (shaderNode && shaderNode.hasAttribute("script"))
     	{
     		var scriptURL = shaderNode.getAttribute("script");
@@ -120,7 +120,7 @@
                 sources.vertex = vsScript.textContent;
                 sources.fragment = fsScript.textContent;
     			}
-    
+
                 shader = this.createShaderFromSources(sources);
     		}
     		shader.hasTransparency = flags.hasTransparency;
@@ -130,7 +130,7 @@
     		shader = this.shaders["defaultShader"];
     		shaderId = "defaultShader";
     	}
-    
+
     	if (shaderAdapter) {
     		var texturesCreated = this.createTextures(shader, shaderAdapter);
     		if (!texturesCreated) {
@@ -147,22 +147,22 @@
     			this.setUniformVariables(shader, dataTable);
     		}
     	}
-    
+
        return shaderId;
     };
-    
+
     XML3DShaderManager.prototype.getStandardShaderSource = function(scriptURL, sources, shaderAdapter, lights, flags) {
     	//Need to check for textures to decide which internal shader to use
     var sources = {vertex:null, fragment:null};
     var shaderName = scriptURL.substring(scriptURL.lastIndexOf(':')+1);
-    
+
     //Need to check for textures to decide which internal shader to use
-    if (hasTextures && (shaderName == "phong" || shaderName == "diffuse"))
+    if (flags.hasTextures && (shaderName == "phong" || shaderName == "diffuse"))
         shaderName = "textured"+shaderName;
-    
+
     	if (flags.hasVColors)
         shaderName += "vcolor";
-    
+
     switch (shaderName) {
     case "phong":
     case "texturedphong":
@@ -181,57 +181,57 @@
     	    sources.fragment = maxLights + sources.fragment;
             break;
     	default:
-        sources = XML3D.shaders.getScript(shaderName);            
+        sources = XML3D.shaders.getScript(shaderName);
     	}
     return sources;
     };
-    
+
     XML3DShaderManager.prototype.getStandardShaderProgram = function(name) {
     	var sources = {};
-    
+
     sources = XML3D.shaders.getScript(name);
     if (!sources || !sources.vertex) {
         sources = {};
         XML3D.debug.logError("Unknown shader: "+name+". Using flat shader instead.");
     	}
-    
+
     	var shaderProgram = this.createShaderFromSources(sources);
-    
+
     	return shaderProgram;
     };
-    
+
     XML3DShaderManager.prototype.createShaderFromSources = function(sources) {
     	var gl = this.gl;
-    
+
     if (!sources.vertex || !sources.fragment) {
     		return this.shaders["defaultShader"];
     	}
-    
+
     	var prg = gl.createProgram();
-    
+
     var vShader = this.compileShader(gl.VERTEX_SHADER, sources.vertex);
     var fShader = this.compileShader(gl.FRAGMENT_SHADER, sources.fragment);
-    
+
     	if (vShader === null || fShader === null) {
     		//Use a default flat shader instead
     		return this.shaders["defaultShader"];
     	}
-    
+
     	//Link shader program
     	gl.attachShader(prg, vShader);
     	gl.attachShader(prg, fShader);
     	gl.linkProgram(prg);
-    
+
     	if (gl.getProgramParameter(prg, gl.LINK_STATUS) == 0) {
     		var errorString = "Shader linking failed: \n";
     		errorString += gl.getProgramInfoLog(prg);
     		errorString += "\n--------\n";
     		XML3D.debug.logError(errorString);
     		gl.getError();
-    
+
     		return this.shaders["defaultShaders"];
     	}
-    
+
     	var programObject = {
     			attributes 	: {},
     			uniforms 	: {},
@@ -240,9 +240,9 @@
     			vSource		: sources.vs,
     			fSource		: sources.fs
     	};
-    
+
     	gl.useProgram(prg);
-    
+
     	//Tally shader attributes
     	var numAttributes = gl.getProgramParameter(prg, gl.ACTIVE_ATTRIBUTES);
     	for (var i=0; i<numAttributes; i++) {
@@ -255,7 +255,7 @@
     		attInfo.location = gl.getAttribLocation(prg, att.name);
     		programObject.attributes[att.name] = attInfo;
     	}
-    
+
     	//Tally shader uniforms and samplers
     	var texCount = 0;
     	var numUniforms = gl.getProgramParameter(prg, gl.ACTIVE_UNIFORMS);
@@ -267,7 +267,7 @@
     		uniInfo.size = uni.size;
     		uniInfo.glType = uni.type;
     		uniInfo.location = gl.getUniformLocation(prg, uni.name);
-    
+
     		if (uni.type == gl.SAMPLER_2D || uni.type == gl.SAMPLER_CUBE) {
     			uniInfo.texUnit = texCount;
     			programObject.samplers[uni.name] = uniInfo;
@@ -276,37 +276,37 @@
     		else
     			programObject.uniforms[uni.name] = uniInfo;
     	}
-    
+
     	this.setStandardUniforms(programObject);
     	programObject.changes = [];
     	return programObject;
     };
-    
+
     XML3DShaderManager.prototype.compileShader = function(type, shaderSource) {
     	var gl = this.gl;
-    
+
     	var shd = gl.createShader(type);
     	gl.shaderSource(shd, shaderSource);
     	gl.compileShader(shd);
-    
+
     	if (gl.getShaderParameter(shd, gl.COMPILE_STATUS) == 0) {
     		var errorString = "";
     		if (type == gl.VERTEX_SHADER)
     			errorString = "Vertex shader failed to compile: \n";
     		else
     			errorString = "Fragment shader failed to compile: \n";
-    
+
     		errorString += gl.getShaderInfoLog(shd) + "\n--------\n";
     		XML3D.debug.logError(errorString);
     		gl.getError();
-    
+
     		return null;
     	}
-    
+
     	return shd;
     };
-    
-    
+
+
     XML3DShaderManager.prototype.recompileShader = function(shaderAdapter, lights) {
     	var shaderName = shaderAdapter.node.id;
     	var shader = this.shaders[shaderName];
@@ -316,10 +316,10 @@
     		this.createShader(shaderAdapter, lights);
     	}
     };
-    
+
     XML3DShaderManager.prototype.shaderDataChanged = function(adapter, attrName, newValue, textureName) {
     	var shader = this.shaders[adapter.node.id];
-    
+
     	//Store the change, it will be applied the next time the shader is bound
     	if (attrName == "src") {
     		//A texture source was changed
@@ -329,39 +329,39 @@
     				shader.samplers[textureName] = this.replaceTexture(adapter, sampler);
     		} else
     			XML3D.debug.logError("Couldn't apply change because of a missing texture name");
-    
+
     	} else {
     		if (attrName == "transparency")
     			shader.hasTransparency = newValue > 0;
-    
+
     		shader.changes.push({
     			type : "uniform",
     			name : attrName,
     			newValue : newValue
     		});
     	}
-    
+
     };
-    
-    
+
+
     XML3DShaderManager.prototype.setStandardUniforms = function(sp) {
-    
+
     	var gl = this.gl;
-    
+
     	var uniform = null;
-    
+
     	//Diffuse color
     	uniform = sp.uniforms.diffuseColor;
     	if (uniform) {
     		this.setUniform(gl, uniform, [1.0, 1.0, 1.0]);
     	}
-    
+
     	//Emissive color
     	uniform = sp.uniforms.emissiveColor;
     	if (uniform) {
     		this.setUniform(gl, uniform, [0.0, 0.0, 0.0]);
     	}
-    
+
     	//Specular color
     	uniform = sp.uniforms.specularColor;
     	if (uniform) {
@@ -371,24 +371,24 @@
     	if(uniform) {
             this.setUniform(gl, sp.uniforms.useSpecularTexture, 0);
         }
-        
-    
+
+
     	//Shininess
     	uniform = sp.uniforms.shininess;
     	if (uniform) {
     		this.setUniform(gl, uniform, 0.2);
     	}
-    
+
     	//Transparency
     	uniform = sp.uniforms.transparency;
     	if (uniform) {
     		this.setUniform(gl, uniform, 0.0);
     	}
-    
-    
+
+
     	//XML3D.webgl.checkError(this.gl);
     };
-    
+
     XML3DShaderManager.prototype.getShaderById = function(shaderId) {
     	var sp = this.shaders[shaderId];
     	if (!sp) {
@@ -400,41 +400,41 @@
     			if (this.shaders[shaderId])
     				return this.shaders[shaderId];
     		}
-    
+
     		XML3D.debug.logError("Could not find the shader [ "+shaderId+" ]");
     		sp = this.shaders["default"];
     	}
     	return sp;
     };
-    
+
     XML3DShaderManager.prototype.setUniformVariables = function(shader, uniforms) {
     	this.bindShader(shader);
-    
+
     	for (var name in uniforms) {
     		var u = uniforms[name];
-    
+
     		if (u.value)
     			u = u.value;
     		if (u.clean)
     			continue;
     		if (u.length == 1)
     			u = u[0]; // Either a single float, int or bool
-    
+
     		if (shader.uniforms[name]) {
     			this.setUniform(this.gl, shader.uniforms[name], u);
     		}
     	}
-    
+
     };
-    
+
     XML3DShaderManager.prototype.bindShader = function(shader) {
         var sp = (typeof shader == typeof "") ? this.getShaderById(shader) : shader;
-    
+
         if (this.currentProgram != sp.handle) {
             this.currentProgram = sp.handle;
             this.gl.useProgram(sp.handle);
         }
-    
+
         var samplers = sp.samplers;
     	for (var tex in samplers) {
     		this.bindTexture(samplers[tex]);
@@ -443,7 +443,7 @@
             }
     	}
     };
-    
+
     XML3DShaderManager.prototype.updateShader = function(sp) {
         this.bindShader(sp);
         //Apply any changes encountered since the last time this shader was rendered
@@ -455,7 +455,7 @@
         }
         sp.changes = [];
     };
-    
+
     XML3DShaderManager.prototype.unbindShader = function(shader) {
         // TODO: unbind samplers (if any)
     	var sp = (typeof shader == typeof "") ? this.getShaderById(shader) : shader;
@@ -463,11 +463,11 @@
     	for (var tex in samplers) {
     		this.unbindTexture(samplers[tex]);
     	}
-    
+
     	this.currentProgram = null;
     	this.gl.useProgram(null);
     };
-    
+
     var rc = window.WebGLRenderingContext;
     XML3DShaderManager.prototype.setUniform = function(gl, u, value) {
     	switch (u.glType) {
@@ -475,56 +475,56 @@
             case rc.INT:
             case rc.SAMPLER_2D:
                 gl.uniform1i(u.location, value); break;
-    
+
     		case 35671: 														//gl.BOOL_VEC2
     		case 35667:	gl.uniform2iv(u.location, value); break;				//gl.INT_VEC2
-    
+
     		case 35672:															//gl.BOOL_VEC3
     		case 35668:	gl.uniform3iv(u.location, value); break;				//gl.INT_VEC3
-    
+
     		case 35673:															//gl.BOOL_VEC4
     		case 35669:	gl.uniform4iv(u.location, value); break;				//gl.INT_VEC4
-    
+
     		case 5126:	gl.uniform1f(u.location, value); break;					//gl.FLOAT
     		case 35664:	gl.uniform2fv(u.location, value); break;				//gl.FLOAT_VEC2
     		case 35665:	gl.uniform3fv(u.location, value); break;				//gl.FLOAT_VEC3
     		case 35666:	gl.uniform4fv(u.location, value); break;				//gl.FLOAT_VEC4
-    
+
     		case 35674: gl.uniformMatrix2fv(u.location, gl.FALSE, value); break;//gl.FLOAT_MAT2
     		case 35675: gl.uniformMatrix3fv(u.location, gl.FALSE, value); break;//gl.FLOAT_MAT3
     		case 35676: gl.uniformMatrix4fv(u.location, gl.FALSE, value); break;//gl.FLOAT_MAT4
-    
+
     		default:
     			XML3D.debug.logError("Unknown uniform type "+u.glType);
     			break;
     	}
     };
-    
+
     XML3DShaderManager.prototype.setGLContext = function(gl) {
     	this.gl = gl;
     };
-    
+
     XML3DShaderManager.prototype.destroyShader = function(shader) {
     	for (var tex in shader.samplers) {
     		this.destroyTexture(shader.samplers[tex]);
     	}
-    
+
     	this.gl.deleteProgram(shader.handle);
     };
-    
+
     XML3DShaderManager.prototype.createTextures = function(shader, shaderAdapter) {
     	var texUnit = 0;
     	var nameArray = [];
-    
+
     	for (var name in shader.samplers) {
     		nameArray.push(name);
     	}
     	var dataTable = shaderAdapter.requestData(nameArray);
-    
+
     	for (var name in shader.samplers) {
     	    var sampler = shader.samplers[name];
             var texture = dataTable[name];
-            
+
             if (!texture) {
     			//XML3D.debug.logWarning("Can't find required texture with name='"+name+"'. Using default shader instead.");
     			sampler.info = new InvalidTexture();
@@ -535,10 +535,10 @@
     		this.createTexture(dtopt, sampler, texUnit);
     		texUnit++;
     	}
-    
+
     	return true;
     };
-    
+
     XML3DShaderManager.prototype.createTexture = function(dtopt, sampler, texUnit) {
     	if(dtopt.imageAdapter && dtopt.imageAdapter.getValue)
 		{
@@ -564,21 +564,21 @@
 		    XML3D.debug.logWarning("No image found for texture: " + sampler);
 		}
     };
-    
+
     XML3DShaderManager.prototype.replaceTexture = function(adapter, texture) {
     	this.destroyTexture(texture);
     	var dtable = adapter.requestData([texture.name]);
     	var dtopt = dtable[texture.name].getValue();
-    	
+
     	//FIX ME PLEASE
-    	dtopt.imageAdapter.image = null;   	
-    	
+    	dtopt.imageAdapter.image = null;
+
     	this.createTexture(dtopt, texture, texture.texUnit);
-    	
+
     	return texture;
-    	
+
     };
-    
+
     XML3DShaderManager.prototype.createTex2DFromData = function(internalFormat, width, height,
     		sourceFormat, sourceType, texels, opt) {
     	var gl = this.gl;
@@ -591,18 +591,18 @@
     			texels = new Uint8Array(width * height * 4);
     		}
     	}
-    
+
     	var handle = gl.createTexture();
     	gl.bindTexture(gl.TEXTURE_2D, handle);
-    
+
     	//gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, opt.wrapS);
     	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, opt.wrapT);
     	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, opt.minFilter);
     	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, opt.magFilter);
-    
+
     	gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, width, height, 0, sourceFormat, sourceType, texels);
-    
+
     	if (opt.isDepth) {
     		gl.texParameteri(gl.TEXTURE_2D, gl.DEPTH_TEXTURE_MODE,   opt.depthMode);
     		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_COMPARE_MODE, opt.depthCompareMode);
@@ -611,31 +611,31 @@
     	if (opt.generateMipmap) {
     		gl.generateMipmap(gl.TEXTURE_2D);
     	}
-    
+
     	gl.bindTexture(gl.TEXTURE_2D, null);
-    
+
     	info.handle = handle;
     	info.options = opt;
     	info.status = TEXTURE_STATE.VALID;
     	info.glType = gl.TEXTURE_2D;
     	info.format = internalFormat;
-    
+
     	return info;
     };
-    
+
     XML3DShaderManager.prototype.createTex2DFromImage = function(info) {
     	var gl = this.gl;
     	var opt = info.options;
     	var image = info.image;
-    	
+
     	gl.bindTexture(gl.TEXTURE_2D, info.handle);
-    
+
     	//gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, opt.wrapS);
     	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, opt.wrapT);
     	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, opt.minFilter);
     	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, opt.magFilter);
-    
+
     	if (!this.isPowerOfTwo(image.width) || !this.isPowerOfTwo(image.height)) {
     	    // Scale up the texture to the next highest power of two dimensions.
     	    var canvas = document.createElement("canvas");
@@ -646,26 +646,26 @@
     	    //ctx.drawImage(image, 0, 0, image.width, image.height); //centered with transparent padding around edges
     	    image = canvas;
     	}
-    
+
     	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-    
+
     	if (opt.generateMipmap) {
     		gl.generateMipmap(gl.TEXTURE_2D);
     	}
-    
+
     	gl.bindTexture(gl.TEXTURE_2D, null);
-    
+
     	info.status = TEXTURE_STATE.VALID;
     	info.glType = gl.TEXTURE_2D;
     	info.format = gl.RGBA;
-    
+
     	return info;
     };
-    
+
     XML3DShaderManager.prototype.bindTexture = function(tex) {
     	var info = tex.info;
     	var gl = this.gl;
-    	
+
     	switch(info.status) {
     	    case TEXTURE_STATE.VALID:
                 gl.activeTexture(gl.TEXTURE0 + info.unit + 1);
@@ -685,21 +685,21 @@
                 this.setUniform(gl, tex, 0);
     	};
     };
-    
+
     XML3DShaderManager.prototype.unbindTexture = function(tex) {
         this.gl.activeTexture(this.gl.TEXTURE1 + tex.info.unit);
         this.gl.bindTexture(tex.info.glType, null);
     };
-    
+
     XML3DShaderManager.prototype.destroyTexture = function(tex) {
     	if (tex.info && tex.info.handle)
     		this.gl.deleteTexture(tex.info.handle);
     };
-    
+
     XML3DShaderManager.prototype.isPowerOfTwo = function(dimension) {
         return (dimension & (dimension - 1)) == 0;
     };
-    
+
     XML3DShaderManager.prototype.nextHighestPowerOfTwo = function(x) {
         --x;
         for (var i = 1; i < 32; i <<= 1) {

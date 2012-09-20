097d3940d0a27e5851830eb96f2957614a56c4d4
Felix Klein <lachsen@cg.uni-saarland.de>
2012-09-21 00:26:40 +0200

    Fix in xflow tests

--- tests/data-xflow.js
+++ tests/data-xflow.js
@@ -42,7 +42,7 @@ module("Xflow tests", {
     },
 
     loadTestXML : function(url, handler) {
-        this.factory = XML3D.data.factory;
+        this.factory = document.getElementById("xml3dframe").contentWindow.XML3D.data.factory;
         var defsElem = this.doc.getElementById("defsElem");
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.open("GET",url,false);

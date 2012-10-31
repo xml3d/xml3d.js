3d86c3aafc0774fbae24ba930506f4ff2dcb1468
Felix Klein <lachsen@cg.uni-saarland.de>
2012-10-31 15:33:46 +0100

    Fix to avoid error with closure compilation

--- src/xflow/operator/lerp3seq.js
+++ src/xflow/operator/lerp3seq.js
@@ -5,20 +5,6 @@ Xflow.registerOperator("lerpSeq", {
     mapping: [  {source: 'sequence', sequence: Xflow.SEQUENCE.PREV_BUFFER, keySource: 'key'},
                 {source: 'sequence', sequence: Xflow.SEQUENCE.NEXT_BUFFER, keySource: 'key'},
                 {source: 'sequence', sequence: Xflow.SEQUENCE.LINEAR_WEIGHT, keySource: 'key'}],
-    evaluate: function(result, value1, value2, weight) {
-        var me = this;
-        this.result.result = sequence.interpolate(weight[0], function(v1,v2,t) {
-            if (!me.tmp || me.tmp.length != v1.length)
-                me.tmp = new Float32Array(v1.length);
-            var result = me.tmp;
-            var it = 1.0 - t;
-            for(var i = 0; i < v1.length; i++) {
-                result[i] = v1[i] * it + v2[i] * t;
-            };
-            return result;
-        });
-        return true;
-    },
     evaluate_core: function(result, value1, value2, weight){
         var invWeight = 1 - weight[0];
         result[0] = invWeight*value1[0] + weight[0]*value2[0];

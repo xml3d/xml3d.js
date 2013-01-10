b161b6f5de3acba202c8ff8a12b1f5c6da1fe67a
Dmitri Rubinstein <rubinstein@cs.uni-saarland.de>
2013-01-10 22:32:08 +0100

    Added sepiaImage operator.

---
+++ src/xflow/operator/default/sepiaImage.js
@@ -0,0 +1,33 @@
+Xflow.registerOperator("sepiaImage", {
+    outputs: [ {type: 'texture', name : 'result', customAlloc: true} ],
+    params:  [ {type: 'texture', name : 'image'} ],
+    alloc: function(sizes, image) {
+        var samplerConfig = new Xflow.SamplerConfig;
+        samplerConfig.setDefaults();
+        sizes['result'] = {
+            imageFormat : {width: Math.max(image.width, 1), height: Math.max(image.height, 1)},
+            samplerConfig : samplerConfig
+        };
+    },
+    evaluate: function(result, image) {
+        var s = image.data;
+        var d = result.data;
+        var r = 0, g = 0, b = 0;
+        for(var i = 0 ; i < s.length; i += 4) {
+            r = (s[i] * 0.393 + s[i+1] * 0.769 + s[i+2] * 0.189);
+            g = (s[i] * 0.349 + s[i+1] * 0.686 + s[i+2] * 0.168);
+            b = (s[i] * 0.272 + s[i+1] * 0.534 + s[i+2] * 0.131);
+            if (r>255) r = 255;
+            if (g>255) g = 255;
+            if (b>255) b = 255;
+            if (r<0) r = 0;
+            if (g<0) g = 0;
+            if (b<0) b = 0;
+            d[i] = r;
+            d[i+1] = g;
+            d[i+2] = b;
+            d[i+3] = 255;
+        }
+        return true;
+    }
+});

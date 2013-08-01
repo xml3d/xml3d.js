734c26e1900c1d589dfca9d124624e4294799f5a
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2013-08-01 10:36:34 +0200

    Added a RenderPipeline class that contains targets, render passes and (optionally) shaders.
    Users can create a RenderPipeline and fill it with their own targets and render passes. The pipeline can then be
    set active by getting the renderInterface on the XML3D element and calling setRenderPipeline. Each Pipeline is self
    contained and should be initialized once by calling init() after all render passes have been added.

---
+++ src/renderer/gl/base/pipeline.js
@@ -0,0 +1,65 @@
+(function (webgl) {
+
+    var RenderPipeline = function (context) {
+        this.renderPasses = [];
+        this.targets = {};
+        this.shaders = {};
+        this.context = context;
+    };
+
+    XML3D.extend(RenderPipeline.prototype, {
+
+        init: function() {
+            var context = this.context;
+            this.renderPasses.forEach(function(pass) {
+                if (pass.init) {
+                    pass.init(context);
+                }
+            });
+        },
+
+        addRenderPass: function(pass, position) {
+            if (position !== undefined) {
+                this.renderPasses = this.renderPasses.splice(position, 0, pass);
+            } else {
+                this.renderPasses.push(pass);
+            }
+            pass.setRenderPipeline(this);
+        },
+
+        addRenderTarget: function(name, target) {
+            if (this.targets[name]) {
+                XML3D.debug.logWarning("Render pipeline already contains a target with name '"+name+"', it will be overwritten!");
+            }
+            this.targets[name] = target;
+        },
+
+        getRenderTarget: function(name) {
+            if (name === "screen") {
+                return this.context.canvasTarget;
+            }
+            return this.targets[name];
+        },
+
+        render : function(scene) {
+            for (var i=0; i < this.renderPasses.length; i++) {
+                this.renderPasses[i].render(scene);
+            }
+        },
+
+        getShader: function(name) {
+            return this.shaders[name];
+        },
+
+        addShader: function(name, shader) {
+            this.shaders[name] = shader;
+        }
+
+
+
+
+    });
+
+    webgl.RenderPipeline = RenderPipeline;
+
+})(XML3D.webgl);

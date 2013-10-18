0f11f7a64a4d4d0dd9b88d4e0a436df75c56d964
Kristian Sons <kristian.sons@dfki.de>
2013-10-18 09:30:40 +0200

    Added light depth shader

--- src/renderer/shaders/utility.js
+++ src/renderer/shaders/utility.js
@@ -74,3 +74,38 @@ XML3D.shaders.register("pickedNormals", {
 
     uniforms : {}
 });
+
+XML3D.shaders.register("light-depth", {
+
+    vertex: [
+        "attribute vec3 position;",
+        "varying vec4 worldPosition;",
+        "uniform mat4 modelMatrix;",
+        "uniform mat4 modelViewProjectionMatrix;",
+
+        "void main(void) {",
+        "   worldPosition = modelMatrix * vec4(position, 1.0);",
+        "   gl_Position   = modelViewProjectionMatrix * vec4(position, 1.0);",
+        "}"
+    ].join("\n"),
+
+    fragment: [
+        "varying vec4 worldPosition;",
+        "uniform mat4 viewMatrix;",
+
+        "vec4 pack_depth( const in float depth ) {",
+        "const vec4 bit_shift = vec4( 256.0 * 256.0 * 256.0, 256.0 * 256.0, 256.0, 1.0 );",
+        "const vec4 bit_mask  = vec4( 0.0, 1.0 / 256.0, 1.0 / 256.0, 1.0 / 256.0 );",
+        "vec4 res = fract( depth * bit_shift );",
+        "res -= res.xxyz * bit_mask;",
+        "return res;",
+        "}",
+
+
+        "void main(void) {",
+        "    gl_FragColor = pack_depth( gl_FragCoord.z );",
+        "}"
+    ].join("\n"),
+
+    uniforms: {}
+});

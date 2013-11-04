2d9070cdf94a8055e1f9d0619b37689b701efb68
Kristian Sons <kristian.sons@dfki.de>
2013-11-04 15:57:18 +0100

    More shader override tests
    Includes changes of entries that trigger a recompilation of the shader

--- tests/shader-overrides.js
+++ tests/shader-overrides.js
@@ -1,4 +1,4 @@
-module("Shader overrides", {
+module("Shader overrides (no shader recompilation)", {
     setup : function() {
         stop();
         var that = this;
@@ -111,4 +111,88 @@ test("Uniform override with default shader", 4, function() {
     var override = this.doc.getElementById("override");
     override.parentNode.removeChild(override);
     handler.draw();
+});
+
+
+module("Shader overrides (with shader recompilation)", {
+    setup : function() {
+        stop();
+        var that = this;
+        this.cb = function(e) {
+            ok(true, "Scene loaded");
+            that.doc = document.getElementById("xml3dframe").contentDocument;
+            start();
+        };
+        loadDocument("scenes/shader-overrides02.xhtml"+window.location.search, this.cb);
+    },
+    teardown : function() {
+        var v = document.getElementById("xml3dframe");
+        v.removeEventListener("load", this.cb, true);
+    }
+});
+
+test("Texture overrides", 8, function() {
+    var xml3dElement = this.doc.getElementById("xml3DElem");
+    var win = this.doc.defaultView;
+    var gl = getContextForXml3DElement(xml3dElement);
+    var handler = getHandler(xml3dElement);
+    var testFunc = function(n) {
+        var actual = win.getPixelValue(gl, 40, 150);
+        deepEqual(actual, [ 255, 255, 0, 255 ], "1: wave & texture");
+        actual = win.getPixelValue(gl, 125, 150);
+        deepEqual(actual, [ 255, 0, 0, 255 ], "2: red");
+        actual = win.getPixelValue(gl, 200, 150);
+        deepEqual(actual, [ 87, 87, 87, 255 ], "3: grey");
+        actual = win.getPixelValue(gl, 280, 150);
+        deepEqual(actual, [ 87, 87, 0, 255 ], "4: dark yellow");
+        actual = win.getPixelValue(gl, 372, 150);
+        QUnit.closePixel(actual, [ 86, 86, 86, 255 ], 2, "5: wave");
+        actual = win.getPixelValue(gl, 440, 150);
+        deepEqual(actual, [ 87, 87, 87, 255 ], "6: grey");
+        xml3dElement.removeEventListener("framedrawn", testFunc);
+        start();
+    };
+
+    xml3dElement.addEventListener("framedrawn", testFunc);
+    stop();
+    handler.draw();
+});
+
+test("Texture override changes", 8, function() {
+    var xml3dElement = this.doc.getElementById("xml3DElem");
+    var win = this.doc.defaultView;
+    var gl = getContextForXml3DElement(xml3dElement);
+    var handler = getHandler(xml3dElement);
+    var testFunc = function(n) {
+        var actual = win.getPixelValue(gl, 40, 150);
+        deepEqual(actual, [ 255, 255, 255, 255 ], "1: wave without texture: white");
+        actual = win.getPixelValue(gl, 125, 150);
+        deepEqual(actual, [ 255, 0, 0, 255 ], "2: red");
+        actual = win.getPixelValue(gl, 200, 150);
+        deepEqual(actual, [ 87, 87, 0, 255 ], "3: grey");
+        actual = win.getPixelValue(gl, 280, 150);
+        deepEqual(actual, [ 87, 87, 0, 255 ], "4: dark yellow");
+        actual = win.getPixelValue(gl, 372, 150);
+        QUnit.closePixel(actual, [ 86, 86, 86, 255 ], 2, "5: wave");
+        actual = win.getPixelValue(gl, 440, 150);
+        deepEqual(actual, [ 87, 87, 87, 255 ], "6: grey");
+        xml3dElement.removeEventListener("framedrawn", testFunc);
+        start();
+    };
+
+    handler.draw();
+    var textureElement = xml3dElement.querySelector("#m_1 texture");
+    var mesh3 = xml3dElement.querySelector("#m_3");
+
+    textureElement.parentNode.removeChild(textureElement);
+    mesh3.appendChild(textureElement);
+
+    //this.doc.querySelector("#p_5").textContent = "5: blue";
+    //this.doc.querySelector("#p_6").textContent = "6: green";
+
+
+    xml3dElement.addEventListener("framedrawn", testFunc);
+    stop();
+    handler.draw();
+
 });

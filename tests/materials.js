8aa14e947c6887ac0a3e17644eb0c694a0ec9cb1
Kristian Sons <kristian.sons@dfki.de>
2015-02-18 16:11:54 +0100

    Improved test descriptions

--- tests/materials.js
+++ tests/materials.js
@@ -28,15 +28,15 @@ test("Change material model of mesh", 8, function () {
     });
     var changeShadeInternal = checkShadeInternal.then(doc.defaultView.changeInternalShadeJS).then(promiseSceneRendered.bind(null, scene)).then(function(s) {
         var pick = XML3DUnit.getPixelValue(getContextForXml3DElement(s),256,256);
-        QUnit.closeArray(pick, [255, 0, 255, 255], PIXEL_EPSILON, "Changed script content of shade.js shader. Should be magenta now.");
+        QUnit.closeArray(pick, [255, 0, 255, 255], PIXEL_EPSILON, "Changed script content of internal shade.js shader. Should be magenta now.");
     });
     var checkShadeExternal = changeShadeInternal.then(doc.defaultView.setShadeExternal).then(promiseSceneRendered.bind(null, scene)).then(function(s) {
         var pick = XML3DUnit.getPixelValue(getContextForXml3DElement(s),256,256);
-        QUnit.closeArray(pick, [51, 51, 51, 255], PIXEL_EPSILON, "Changed script to internal shade.js shader. Should be grey now.");
+        QUnit.closeArray(pick, [51, 51, 51, 255], PIXEL_EPSILON, "Changed script to external shade.js shader in XML document. Should be grey now.");
     });
     var checkShadeExternalJS = checkShadeExternal.then(doc.defaultView.setShadeExternalJS).then(promiseSceneRendered.bind(null, scene)).then(function(s) {
         var pick = XML3DUnit.getPixelValue(getContextForXml3DElement(s),256,256);
-        QUnit.closeArray(pick, [255, 255, 0, 255], PIXEL_EPSILON, "Changed script to internal shade.js shader. Should be yellow now.");
+        QUnit.closeArray(pick, [255, 255, 0, 255], PIXEL_EPSILON, "Changed script to external shade.js shader in Javascript document. Should be yellow now.");
     });
     checkShadeExternalJS.fin(QUnit.start).done();
 });

e51e16827ae1a48f3f2dc72ebf6f83d30726f50a
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2015-03-11 13:38:06 +0100

    Remove the Xflow build scripts and module

--- src/xflow/index.js
+++
@@ -1,8 +0,0 @@
-
-//Public Xflow API
-
-//window.Xflow = window.Xflow || {};
-//var Xflow = window.Xflow;
-
-//Xflow.registerOperator = require("./operator/operator.js").registerOperator;
-//Xflow.ComputeRequest = require("./interface/request.js").ComputeRequest;

e10f1978316dbece9c473031936236590a9f022f
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2015-03-12 15:21:00 +0100

    Extra math functions no longer need to be require'd in the renderer

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -3,7 +3,6 @@
     XML3D.shaders = XML3D.materials = require("./webgl/materials/");
     XML3D.webgl = require("./webgl");
     XML3D.renderer = require("./renderer");
-    require("../xflow/utils/math.js")(XML3D.math);
     require("../xflow/operator/default");
 
 }(module));

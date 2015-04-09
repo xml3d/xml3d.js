86e76413252b3a23e03549bd859928f381286e1e
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2015-04-09 13:27:36 +0200

    Remove references to getBoundingBox() in the test library

--- tests/elements-generated.js
+++ tests/elements-generated.js
@@ -24,7 +24,6 @@ test("&lt;xml3d&gt; interface test", function() {
     equal(typeof e.getElementByPoint, "function", "getElementByPoint is of type 'function'");
     equal(typeof e.generateRay, "function", "generateRay is of type 'function'");
     equal(typeof e.getElementByRay, "function", "getElementByRay is of type 'function'");
-    equal(typeof e.getBoundingBox, "function", "getBoundingBox is of type 'function'");
     equal(typeof e.getLocalBoundingBox, "function", "getLocalBoundingBox is of type 'function'");
     equal(typeof e.getWorldBoundingBox, "function", "getWorldBoundingBox is of type 'function'");
     equal(typeof e.activeView, "string", "activeView is of type 'string'");
@@ -71,7 +70,6 @@ test("&lt;group&gt; interface test", function() {
     equal(typeof e.visible, "boolean", "visible is of type 'boolean'");
     equal(typeof e.getWorldMatrix, "function", "getWorldMatrix is of type 'function'");
     equal(typeof e.getLocalMatrix, "function", "getLocalMatrix is of type 'function'");
-    equal(typeof e.getBoundingBox, "function", "getBoundingBox is of type 'function'");
     equal(typeof e.getLocalBoundingBox, "function", "getLocalBoundingBox is of type 'function'");
     equal(typeof e.getWorldBoundingBox, "function", "getWorldBoundingBox is of type 'function'");
     equal(typeof e.transform, "string", "transform is of type 'string'");
@@ -97,7 +95,6 @@ test("&lt;mesh&gt; interface test", function() {
     equal(typeof e.type, "string", "type is of type 'string'");
     equal(typeof e.compute, "string", "compute is of type 'string'");
     equal(typeof e.getWorldMatrix, "function", "getWorldMatrix is of type 'function'");
-    equal(typeof e.getBoundingBox, "function", "getBoundingBox is of type 'function'");
     equal(typeof e.getLocalBoundingBox, "function", "getLocalBoundingBox is of type 'function'");
     equal(typeof e.getWorldBoundingBox, "function", "getWorldBoundingBox is of type 'function'");
     equal(typeof e.getOutputNames, "function", "getOutputNames is of type 'function'");
--- tests/elements-methods.js
+++ tests/elements-methods.js
@@ -7,9 +7,6 @@ test("xml3d methods test", function() {
     ok(node.generateRay(0,0) instanceof XML3DRay, "xml3d::generateRay returns ");
     equal(node.getElementByRay(new XML3DRay(), new XML3DVec3(), new XML3DVec3()), null, "xml3d::getElementByRay returns ");
 
-    ok(node.getBoundingBox() instanceof XML3DBox, "xml3d::getBoundingBox returns ");
-    ok(node.getBoundingBox().isEmpty(), "Empty xml3d delivers empty BoundingBox");
-
     ok(node.getLocalBoundingBox() instanceof XML3DBox, "xml3d::getLocalBoundingBox returns ");
     ok(node.getLocalBoundingBox().isEmpty(), "Empty xml3d delivers empty BoundingBox");
 
@@ -28,9 +25,6 @@ test("group interface test", function() {
     deepEqual(node.getLocalMatrix(), new XML3DMatrix(),
             "group::getLocalMatrix returns identity matrix with default attributes.");
 
-    ok(node.getBoundingBox() instanceof XML3DBox, "group::getBoundingBox returns XML3DBox");
-    ok(node.getBoundingBox().isEmpty(), "Empty group delivers empty BoundingBox");
-
     ok(node.getLocalBoundingBox() instanceof XML3DBox, "group::getLocalBoundingBox returns ");
     ok(node.getLocalBoundingBox().isEmpty(), "Empty group delivers empty BoundingBox");
 
@@ -44,9 +38,6 @@ test("mesh interface test", function() {
     ok(node.getWorldMatrix() instanceof XML3DMatrix, "mesh::getWorldMatrix returns XML3DMatrix");
     deepEqual(node.getWorldMatrix(), new XML3DMatrix(),
             "mesh::getWorldMatrix returns identity matrix if not in hierarchy.");
-
-    ok(node.getBoundingBox() instanceof XML3DBox, "mesh::getBoundingBox returns XML3DBox");
-    ok(node.getBoundingBox().isEmpty(), "Empty mesh delivers empty BoundingBox");
 
     ok(node.getLocalBoundingBox() instanceof XML3DBox, "mesh::getLocalBoundingBox returns ");
     ok(node.getLocalBoundingBox().isEmpty(), "Empty mesh delivers empty BoundingBox");

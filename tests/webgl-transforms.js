3ad5ab57a32f446433723f9d1a150b8675cb4e6e
Kristian Sons <kristian.sons@dfki.de>
2015-04-10 14:56:07 +0200

    Tests: Made two transform tests more reliable based on promises

--- tests/webgl-transforms.js
+++ tests/webgl-transforms.js
@@ -17,41 +17,32 @@ module("Transformations", {
 });
 
 test("Local Transformations", 4, function() {
-    var xTest = this.doc.getElementById("xml3dTest"),
-        glTest = getContextForXml3DElement(xTest), hTest = getHandler(xTest);
-    var self = this;
+    var xTest = this.doc.getElementById("xml3dTest");
+    stop();
 
     var trans = this.doc.getElementById("group_transform2");
     var css = this.doc.getElementById("css_transform2");
     var xflow = this.doc.getElementById("xflow_tranform2");
 
-    function onFrameDrawn(){
+    promiseOneSceneCompleteAndRendered(xTest).then(function() {
         QUnit.closeMatrix(css.getLocalMatrix(), trans.getLocalMatrix(), EPSILON, "CSS and <transform> equal");
         QUnit.closeMatrix(xflow.getLocalMatrix(), trans.getLocalMatrix(), EPSILON, "CSS and <data> equal");
-        start();
-    }
-    xTest.addEventListener("framedrawn", onFrameDrawn);
-    stop();
-    hTest.draw();
+    }).fin(QUnit.start).done();
+
 });
 
 test("Global Transformations", 4, function() {
-    var xTest = this.doc.getElementById("xml3dTest"),
-        glTest = getContextForXml3DElement(xTest), hTest = getHandler(xTest);
-    var self = this;
+    var xTest = this.doc.getElementById("xml3dTest");
+    stop();
 
     var trans = this.doc.getElementById("group_transform1");
     var css = this.doc.getElementById("css_transform1");
     var xflow = this.doc.getElementById("xflow_tranform1");
 
-    function onFrameDrawn(){
+    promiseOneSceneCompleteAndRendered(xTest).then(function() {
         QUnit.closeMatrix(css.getWorldMatrix(), trans.getWorldMatrix(), EPSILON, "CSS and <transform> equal");
         QUnit.closeMatrix(xflow.getWorldMatrix(), trans.getWorldMatrix(), EPSILON, "CSS and <data> equal");
-        start();
-    }
-    xTest.addEventListener("framedrawn", onFrameDrawn);
-    stop();
-    hTest.draw();
+    }).fin(QUnit.start).done();
 });
 
 test("Redraw triggers", 5, function() {

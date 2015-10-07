8427380f29efddfdf1ef5e730fceaa69a2cb98e1
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2015-10-07 12:46:30 +0200

    Fixes #146: Add OES_texture_float_linear to the list of requested extensions

--- src/renderer/webgl/base/context.js
+++ src/renderer/webgl/base/context.js
@@ -46,6 +46,7 @@ EXTENSIONS.MULTIPLE_RENDER_TARGETS = 'WEBGL_draw_buffers';
 EXTENSIONS.DEPTH_TEXTURE = 'WEBGL_depth_texture';
 EXTENSIONS.FLOAT_COLOR_BUFFER = 'WEBGL_color_buffer_float';
 EXTENSIONS.FLOAT_TEXTURES = 'OES_texture_float';
+EXTENSIONS.FLOAT_TEXTURES_LINEAR = "OES_texture_float_linear";
 EXTENSIONS.UINT32_INDICES = 'OES_element_index_uint'; //Also defined in xflow/utils/utils.js
 
 XML3D.extend(GLContext.prototype, {

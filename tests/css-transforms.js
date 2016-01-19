e946443642dbcfe2d2a6f5c2ee492c99be113387
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2016-01-19 10:57:17 +0100

    Add a test for basic support of CSS transitions for transforms

--- tests/css-transforms.js
+++ tests/css-transforms.js
@@ -47,13 +47,34 @@ test("matrix3d with e notation", function() {
 
     var test = frameLoaded.then(function(doc) {
         group = doc.getElementById("rootGroup");
-        console.log(group.getLocalMatrix());
         group.setAttribute("style", "transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 20e-1, 0, -0.1e2, 1)");
-        console.log(group.getLocalMatrix());
         return doc.getElementById("xml3dTest");
     }).then(promiseSceneRendered).then(function(s) {
         var actual = XML3DUnit.getPixelValue(getContextForXml3DElement(s), 300, 100);
         deepEqual(actual, [255, 0, 0, 255], "Group was moved to the right");
+    });
+    test.fin(QUnit.start).done();
+});
+
+test("CSS transitions", function() {
+    stop();
+    var frameLoaded = Q.fcall(promiseIFrameLoaded, "scenes/css-transforms.html");
+    var group;
+
+    var test = frameLoaded.then(function(doc) {
+        group = doc.getElementById("secondGroup");
+        group.setAttribute("style", "transform: translate3d(0,0,-10px);");
+        return doc.getElementById("xml3dTest");
+    }).then(promiseSceneRendered).then(function(s) {
+        group.setAttribute("style", "transform: translate3d(-3px, 0, -10px); transition: transform 100ms linear;");
+        return s;
+    }).then(function(s) {
+        return new Promise(function(resolve, reject) {
+            setTimeout(resolve.bind(resolve, s), 50);
+        });
+    }).then(function(s) {
+        var actual = XML3DUnit.getPixelValue(getContextForXml3DElement(s), 129, 110);
+        deepEqual(actual, [255, 0, 0, 255], "Group was animated to new position");
     });
     test.fin(QUnit.start).done();
 });

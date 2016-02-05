d79ad0d8736ab232be5dec8c6c0de67cf47d18ec
Christian Schlinkmann <christian.schlinkmann@dfki.de>
2016-02-05 10:28:46 +0100

    Override document.registerElement to also remove and re-add any web components that were not registered at time of initialization

--- tests/web-components.js
+++ tests/web-components.js
@@ -8,14 +8,26 @@ test("Simple square component", function() {
     var frameLoaded = Q.fcall(promiseIFrameLoaded, "scenes/web-components.html");
 
     var test = frameLoaded.then(function(doc) { return doc.querySelector("xml3d") }).then(promiseSceneRendered).then(function (s) {
+        // Workaround for Chrome bug https://bugs.webkit.org/show_bug.cgi?id=14563
+        // computed style is incorrect the first time it's queried after removing and re-adding a node to the scene if this query happens
+        // as part of the mutation event processing. This happens as part of the document.registerElement function that we overwrite in dom.js,
+        // causing the returned transform style to be "none" during initialization of the x-square's GroupRenderAdapter.
+        // Firefox does not show this behavior and instead returns the correct style.
+        s.ownerDocument.querySelector("x-square").setAttribute("style", "transform: translate3d(-3px, 0, -10px)");
+        return s;
+    }).then(promiseSceneRendered).then(function(s) {
+        var pick = XML3DUnit.getPixelValue(getContextForXml3DElement(s),70,100);
+        QUnit.closeArray(pick, [0, 255, 0, 255], PIXEL_EPSILON, "Component already in DOM at page load was rendered");
+
         var square = s.ownerDocument.createElement("x-square");
         var group = s.ownerDocument.getElementById("rootGroup");
         group.appendChild(square);
         return s;
     }).then(promiseSceneRendered).then(function(s) {
         var pick = XML3DUnit.getPixelValue(getContextForXml3DElement(s),150,100);
-        QUnit.closeArray(pick, [0, 255, 0, 255], PIXEL_EPSILON, "Green square component was rendered");
+        QUnit.closeArray(pick, [0, 255, 0, 255], PIXEL_EPSILON, "Javascript added component was rendered");
     });
+
 
 
     test.fin(QUnit.start).done();

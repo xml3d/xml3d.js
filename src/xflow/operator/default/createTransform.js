9eeee5aaf446fffb4a12f063c24c0ce2c441760f
Stefan Lemme <stefan.lemme@dfki.de>
2016-03-01 12:48:57 +0100

    fixes for xflow.createTransform() operator

--- src/xflow/operator/default/createTransform.js
+++ src/xflow/operator/default/createTransform.js
@@ -3,6 +3,7 @@
     var TMP_VEC = XML3D.math.vec3.create();
     var TMP_QUAT = XML3D.math.quat.create();
     var mat4 = XML3D.math.mat4;
+    var quat = XML3D.math.quat;
 
     Xflow.registerOperator("xflow.createTransform", {
         outputs: [{type: 'float4x4', name: 'result'}],
@@ -16,20 +17,23 @@
                 var dest = result.subarray(i * 16);
                 mat4.identity(dest);
                 translation && mat4.translate(dest, dest, info.iterFlag[0] ? translation.subarray(i*3) : translation);
-                center && mat4.translate(dest, dest, info.iterFlag[0] ? center.subarray(i*3) : center);
+                center && mat4.translate(dest, dest, info.iterFlag[3] ? center.subarray(i*3) : center);
                 if (rotation) {
-                    mat4.fromRotationTranslation(TMP_MATRIX, info.iterFlag[1] ? rotation.subarray(i*4) : rotation, [0, 0, 0]);
+                    mat4.fromQuat(TMP_MATRIX, info.iterFlag[1] ? rotation.subarray(i*4) : rotation);
                     mat4.multiply(dest, dest, TMP_MATRIX);
                 }
-                if (scaleOrientation) {
-                    mat4.fromRotationTranslation(TMP_MATRIX, info.iterFlag[4] ? scaleOrientation.subarray(i*4) : scaleOrientation, [0, 0, 0]);
-                    mat4.multiply(dest, dest, TMP_MATRIX);
-                }
-                scale && mat4.scale(dest, dest, info.iterFlag[2] ? scale.subarray(i*3) : scale);
-                if (scaleOrientation) {
-                    XML3D.math.quat.copy(TMP_QUAT, info.iterFlag[4] ? scaleOrientation.subarray(i*4) : scaleOrientation)
-                    mat4.fromRotationTranslation(TMP_MATRIX, XML3D.math.quat.negate(TMP_QUAT, TMP_QUAT), [0, 0, 0]);
-                    mat4.multiply(dest, dest, TMP_MATRIX);
+                if (scale) {
+                    if (scaleOrientation) {
+                        quat.copy(TMP_QUAT, info.iterFlag[4] ? scaleOrientation.subarray(i*4) : scaleOrientation)
+                        mat4.fromQuat(TMP_MATRIX, TMP_QUAT);
+                        mat4.multiply(dest, dest, TMP_MATRIX);
+                    }
+                    mat4.scale(dest, dest, info.iterFlag[2] ? scale.subarray(i * 3) : scale);
+                    if (scaleOrientation) {
+                        quat.invert(TMP_QUAT, TMP_QUAT);
+                        mat4.fromQuat(TMP_MATRIX, TMP_QUAT);
+                        mat4.multiply(dest, dest, TMP_MATRIX);
+                    }
                 }
 
                 center && mat4.translate(dest, dest, XML3D.math.vec3.negate(TMP_VEC, info.iterFlag[3] ? center.subarray(i*3) : center));
